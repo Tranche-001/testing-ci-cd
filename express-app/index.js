867369dda7d0513b20ad465b12ce1848bdda53a7
Tranche-001 <tranche100@gmail.com>
2025-12-17 16:27:28 -0300

    update index and docker-compose

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Toad!</h1>')
+  res.send('<h1>Palutena!</h1>')
 })
 
 const PORT = 8080
